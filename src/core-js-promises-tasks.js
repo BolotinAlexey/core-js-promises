be9f45d3c4a796431b77373dcf1d46b8c6e7f42b
Oleksii Bolotin <samkiral73@gmail.com>
2024-04-11 20:04:55 +0200

    add: queuPromises complete

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -149,8 +149,11 @@ async function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+async function queuPromises(promises) {
+  return promises.reduce(
+    async (acc, pr) => (await acc) + (await pr),
+    Promise.resolve('')
+  );
 }
 
 module.exports = {
